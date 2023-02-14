18ff0c9519ed33df6879f800f223dfe8319966ce
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-02-14 13:40:06 +0100

    Always return an array of problems in report

--- lib/puppet-lint.rb
+++ lib/puppet-lint.rb
@@ -170,6 +170,8 @@ class PuppetLint
   # Returns array of problem.
   def report(problems)
     json = []
+    print_stdout = !(configuration.json || configuration.sarif)
+
     problems.each do |message|
       next if message[:kind] == :ignored && !PuppetLint.configuration.show_ignored
 
@@ -179,9 +181,9 @@ class PuppetLint
 
       message[:context] = get_context(message) if configuration.with_context
 
-      if configuration.json || configuration.sarif
-        json << message
-      else
+      json << message
+
+      if print_stdout
         format_message(message)
         print_github_annotation(message) if configuration.github_actions
       end
